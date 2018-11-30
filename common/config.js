2a7e8fe014680b46440173419b0fc1b468140c5a
longlei6666 <894640485@qq.com>
2018-11-30 16:00:42 +0800

    合并公共修改

--- common/config.js
+++ common/config.js
@@ -1,12 +1,18 @@
 //prd  线上		cs 测试  
-var Edition = 'prd';
+var Edition = 'qty';
 var server;
-if(Edition == 'prd'){
-	server  = 'http://192.168.1.107:5006/shanhe-organization/'
+if(Edition == 'qty'){
+	server  = 'http://192.168.1.104:5006/shanhe-mechanism/'
 }else{
-	server  = 'http://192.168.1.116:8080/shanhe-organization/'
+	server  = 'http://192.168.1.190:5006/fenghao-test/'
 }
-
+/* 
+接口前缀解释
+get 获取
+update 更新
+insert  插入
+del  删除
+*/
 var CmsConfig = {
 
     ServiceUrl: {
@@ -21,17 +27,33 @@ var CmsConfig = {
             createCode: "login/createCode",
             // 登录
             login: "login/loginJG",
+            //退出
+            exitLogin:"login/exitLogin"
         },
         //机构后台接口
         Mechanism: {
             //---系统设置
             //基本设置
+            //获取
+            getInstitutionRegisterInfo:"Institution/getInstitutionRegisterInfo",
+            //更新
+            updateInstitutionRegisterInfo:"Institution/updateInstitutionRegisterInfo",
             //邮件设置
+            getInstitutionMailboxSetting:"InstitutionMailboxSetting/getInstitutionMailboxSetting",
+            updateInstitutionMailboxSetting:"InstitutionMailboxSetting/updateInstitutionMailboxSetting",
+            insertInstitutionMailboxSetting:"InstitutionMailboxSetting/insertInstitutionMailboxSetting",
             //短信设置
+            getInstitutionSMSSetting:"InstitutionSMSSetting/getInstitutionSMSSetting",
+            updateSMSSetting:"InstitutionSMSSetting/updateSMSSetting",
+            insertSMSS:"InstitutionSMSSetting/insertSMSS",
             //修改密码
-
+            updatePassword:"login/updatePassword",
             //管理员管理列表
+            getInstitutionStaff:"InstitutionStaff/getInstitutionStaff",
             //添加管理员
+            insertInstitutionStaff:"InstitutionStaff/insertInstitutionStaff",
+            insterRiskSetUp:"InstitutionRiskSetUp/insterRiskSetUp",
+            updateRiskSetUp:"InstitutionRiskSetUp/updateRiskSetUp",
             //修改管理员密码
             //删除管理员
 
@@ -51,6 +73,7 @@ var CmsConfig = {
             */
             //添加商户
             //风控设置
+            getRiskSetUp:"InstitutionRiskSetUp/getRiskSetUp",
             //登录记录
             //商户操作日志，列表
 
@@ -69,6 +92,7 @@ var CmsConfig = {
             //资金变动列表接口
 
             //--全部订单
+            getTransactionReceiptList:"TransactionReceipt/getTransactionReceiptList",
             //全部订单查询
             //列表操作接口：查看、同步
             //成功订单、未支付订单、异常订单同上
@@ -89,12 +113,16 @@ var CmsConfig = {
 
 
             //--广告管理
-            //广告列表
+            insertInstitutionAdvertisement:"InstitutionAdvertisement/insertInstitutionAdvertisement",
+             //广告列表"
+            getInstitutionAdvertisementList:"InstitutionAdvertisement/getInstitutionAdvertisementList",
             //广告列表操作：
             //状态的开/关
+            updateInstitutionAdvertisementState:"InstitutionAdvertisement/updateInstitutionAdvertisementState",
             //编辑接口
+            updateInstitutionAdvertisement:"InstitutionAdvertisement/updateInstitutionAdvertisement",
             //删除接口
-
+            delInstitutionAdvertisementState:"InstitutionAdvertisement/delInstitutionAdvertisementState",
 
             //--财务分析
             //查询当日统计
